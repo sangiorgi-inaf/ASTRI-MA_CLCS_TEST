b13ef4d96306595da2cf225ca44119227178003a
Pierluca Sangiorgi <sangiorgi@ifc.inaf.it>
2019-10-30 12:58:16 +0100

    temp upgrade

---
+++ Astri Launcher/src/it/inaf/iasfpa/astri/camera/launcher/LauncherProperty.java
@@ -0,0 +1,13 @@
+package it.inaf.iasfpa.astri.camera.launcher;
+
+public class LauncherProperty {
+
+	public static final String SERVER_SHELL_PROCESS_NAME = "astri_camera_control_server_sh";
+	public static final String SERVER_JAVA_PROCESS_NAME = "astri_camera_control_server";
+	public static final String SERVER_LAUNCH_SHELL_FILE = "./start_bee_server.sh";
+
+	public static final String CLIENT_SHELL_PROCESS_NAME = "astri_camera_control_client_sh";
+	public static final String CLIENT_JAVA_PROCESS_NAME = "astri_camera_control_client";
+	public static final String CLIENT_LAUNCH_SHELL_FILE = "./start_client.sh";
+	
+}
